1ab1677d0133753b0f509f6ed7bc4ee421f1650b
Lauren Black <laurenblack5280@gmail.com>
2022-07-09 11:44:33 -0600

    meals

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,45 @@
-function nameMenuItem() {
-
+function nameMenuItem(menuItemName) {
+  return "Delicious " + menuItemName
 }
 
+function createMenuItem(name, price, type) {
+  var menuItem = {
+    name: name,
+    price: price,
+    type: type
+  }
+  return menuItem
+}
+
+function addIngredients(newIngredient, ingredients) {
+  if (!ingredients.includes(newIngredient)) {
+    ingredients.push(newIngredient)
+  }
+}
+
+function formatPrice(initialPrice) {
+  return "$" + initialPrice
+}
+
+function decreasePrice(price) {
+  var percentage = price * .10
+  return price - percentage
+}
+
+function createRecipe(title, ingredients, type) {
+  var recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: type
+  }
+  return recipe
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
